4c264745e5fc402f14f4f3464a35d67cbcd9c6a6
Tysonium <tyson.daugherty@gmail.com>
2015-04-06 20:14:00 -0700

    new site basics

--- _site/js/custom.js
+++ _site/js/custom.js
@@ -1,42 +1,191 @@
-(function ($) {
+/*Theme    : assan
+ * Author  : Design_mylife
+ * Version : V1.4
+ * 
+ */
 
-	new gnMenu( document.getElementById( 'gn-menu' ) );
+
+//sticky header on scroll
+$(document).ready(function () {
+    $(window).load(function () {
+        $(".sticky").sticky({topSpacing: 0});
+    });
+});
+
+/* ==============================================
+ WOW plugin triggers animate.css on scroll
+ =============================================== */
+$(document).ready(function () {
+    var wow = new WOW(
+            {
+                boxClass: 'wow', // animated element css class (default is wow)
+                animateClass: 'animated', // animation css class (default is animated)
+                offset: 100, // distance to the element when triggering the animation (default is 0)
+                mobile: false        // trigger animations on mobile devices (true is default)
+            }
+    );
+    wow.init();
+});
 
 
-	//jQuery for page scrolling feature - requires jQuery Easing plugin
-	$(function() {
-		$('.gn-menu li a').bind('click', function(event) {
-			var $anchor = $(this);
-			$('html, body').stop().animate({
-				scrollTop: $($anchor.attr('href')).offset().top
-			}, 1500, 'easeInOutExpo');
-			event.preventDefault();
-		});
-		$('a.scroll').bind('click', function(event) {
-			var $anchor = $(this);
-			$('html, body').stop().animate({
-				scrollTop: $($anchor.attr('href')).offset().top
-			}, 1500, 'easeInOutExpo');
-			event.preventDefault();
-		});
-	});
+//parallax
+$(document).ready(function () {
+    $(window).stellar({
+        horizontalScrolling: false,
+        responsive: true/*,
+         scrollProperty: 'scroll',
+         parallaxElements: false,
+         horizontalScrolling: false,
+         horizontalOffset: 0,
+         verticalOffset: 0*/
+    });
+});
 
-	
-	//nivo lightbox
-	$('.gallery-item a').nivoLightbox({
-		effect: 'fadeScale',                             // The effect to use when showing the lightbox
-		theme: 'default',                           // The lightbox theme to use
-		keyboardNav: true,                          // Enable/Disable keyboard navigation (left/right/escape)
-		clickOverlayToClose: true,                  // If false clicking the "close" button will be the only way to close the lightbox
-		onInit: function(){},                       // Callback when lightbox has loaded
-		beforeShowLightbox: function(){},           // Callback before the lightbox is shown
-		afterShowLightbox: function(lightbox){},    // Callback after the lightbox is shown
-		beforeHideLightbox: function(){},           // Callback before the lightbox is hidden
-		afterHideLightbox: function(){},            // Callback after the lightbox is hidden
-		onPrev: function(element){},                // Callback when the lightbox gallery goes to previous item
-		onNext: function(element){},                // Callback when the lightbox gallery goes to next item
-		errorMessage: 'The requested content cannot be loaded. Please try again later.' // Error message when content can't be loaded
-	});
+//owl carousel for work
+$(document).ready(function () {
 
-	
-})(jQuery);
+    $("#work-carousel").owlCarousel({
+        // Most important owl features
+        items: 4,
+        itemsCustom: false,
+        itemsDesktop: [1199, 3],
+        itemsDesktopSmall: [980, 3],
+        itemsTablet: [768, 3],
+        itemsTabletSmall: false,
+        itemsMobile: [479, 1],
+        singleItem: false,
+        startDragging: true,
+        autoPlay: 6000
+    });
+
+});
+
+
+//owl carousel for news
+$(document).ready(function () {
+
+    $("#news-carousel").owlCarousel({
+        // Most important owl features
+        items: 2,
+        itemsCustom: false,
+        itemsDesktop: [1199, 2],
+        itemsDesktopSmall: [980, 2],
+        itemsTablet: [768, 2],
+        itemsTabletSmall: false,
+        itemsMobile: [479, 1],
+        singleItem: false,
+        startDragging: true,
+        autoPlay: 4000
+    });
+
+});
+
+
+
+//owl carousel for testimonials
+$(document).ready(function () {
+
+    $("#testi-carousel").owlCarousel({
+        // Most important owl features
+        items: 1,
+        itemsCustom: false,
+        itemsDesktop: [1199, 1],
+        itemsDesktopSmall: [980, 1],
+        itemsTablet: [768, 1],
+        itemsTabletSmall: false,
+        itemsMobile: [479, 1],
+        singleItem: false,
+        startDragging: true,
+        autoPlay: 4000
+    });
+
+});
+//featured work carousel slider
+
+$(document).ready(function () {
+
+    $("#featured-work").owlCarousel({
+        autoPlay: 5000, //Set AutoPlay to 3 seconds
+        navigation: true,
+        navigationText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+        pagination: false,
+        items: 4,
+        itemsDesktop: [1199, 3],
+        itemsDesktopSmall: [979, 3],
+        stopOnHover: true
+
+    });
+
+});
+/* ==============================================
+ Counter Up
+ =============================================== */
+jQuery(document).ready(function ($) {
+    $('.counter').counterUp({
+        delay: 100,
+        time: 800
+    });
+});
+
+
+//MAGNIFIC POPUP
+$(document).ready(function () {
+$('.show-image').magnificPopup({type: 'image'});
+});
+
+/* ==============================================
+ flex slider
+ =============================================== */
+
+$(document).ready(function () {
+$('.main-flex-slider,.testi-slide').flexslider({
+    slideshowSpeed: 5000,
+    directionNav: false,
+    animation: "fade"
+});
+});
+//OWL CAROUSEL
+$(document).ready(function () {
+$("#clients-slider").owlCarousel({
+    autoPlay: 3000,
+    pagination: false,
+    items: 4,
+    itemsDesktop: [1199, 3],
+    itemsDesktopSmall: [991, 2]
+});
+});
+
+
+/*========tooltip and popovers====*/
+$(document).ready(function () {
+$("[data-toggle=popover]").popover();
+
+$("[data-toggle=tooltip]").tooltip();
+});
+
+
+
+/* ==============================================
+ mb.YTPlayer
+ =============================================== */
+$(document).ready(function () {
+jQuery(function () {
+    jQuery(".player").mb_YTPlayer();
+});
+});
+
+
+//transparent header
+
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.transparent-header').css("background", "#252525");
+        } else {
+            $('.transparent-header').css("background", "transparent");
+        }
+    });
+});
+
+
+
