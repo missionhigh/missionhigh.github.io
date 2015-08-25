7fef1a865fb916f993e1981991c8553a8cb44b9e
gigmania <tyson.daugherty@gmail.com>
2015-08-24 18:02:51 -0700

    wired the application page

--- _site/README.md
+++ _site/README.md
@@ -1,1 +1,18 @@
-# missionhigh.github.io
+Mission High Foundation
+================
+
+# HACKING
+
+## Mac
+
+Open a Terminal and change to the directory where you've checked out
+`missionbit.github.io`, then run this command to start the [Jekyll]
+preview server:
+
+```bash
+jekyll serve --watch --safe
+```
+
+This will run a webserver on your computer at http://127.0.0.1:4000/
+and automatically rebuild the site when you make changes to the files.
+You will have to reload the pages in your browser to see the changes.
--- _site/js/site.js
+++ _site/js/site.js
@@ -6,8 +6,6 @@ window.jQuery(function ($) {
 			var email = $('#newsletter-email').val();
 			var check = new RegExp("^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*$");
 			var isEmail = check.test(email);
-			console.log(isEmail);
-			console.log(email);
 			$('.subscribe-form')[0].reset();
 
 			if (isEmail) {
@@ -18,13 +16,61 @@ window.jQuery(function ($) {
 				subscriber.set("email", email);
 
 				subscriber.save(null, {
-				  success: function(newsletter) {
+				  success: function(subscriber) {
 				    console.log('save successful');
 				  },
-				  error: function(newsletter, error) {
+				  error: function(subscriber, error) {
 				    console.log('Failed to create new object, with error code: ' + error.description);
 				  }
 				});
 			}
-		})
+		});
+		$("#apply-spark-btn").on('click', function (e) {
+			e.preventDefault();
+			console.log('i have applied');
+			var fullName 				= $('#applicant-name-input').val();
+			var email 					= $('#applicant-email-input').val();
+			var role 						= $('#applicant-role-input').val();
+			var projectTitle 		= $('#applicant-project-input').val();
+			var needStatement 	= $('#need-statement-input').val();
+			var overview 				= $('#overview-input').val();
+			var popServed				= $('#served-input').val();
+			var outcomes 				= $('#outcomes-input').val();
+			var sustainability 	= $('#sustainability-input').val();
+			var budget	 				= $('#budget-input').val();
+			var amount 					= $('.grant-input:checked').val();
+
+			if (fullName && email && role && projectTitle && needStatement && overview && popServed && outcomes && sustainability && budget && amount) {
+				console.log('application is good. no whammys');
+				$('.spark-success-msg').show();
+				$('.spark-error-msg').remove();
+				var Fall15Spark = Parse.Object.extend("Fall15Spark");
+				var fall15Spark = new Fall15Spark();
+				fall15Spark.set("name", fullName);
+				fall15Spark.set("email", email);
+				fall15Spark.set("role", role);
+				fall15Spark.set("project", projectTitle);
+				fall15Spark.set("need", needStatement);
+				fall15Spark.set("overview", overview);
+				fall15Spark.set("population", popServed);
+				fall15Spark.set("outcomes", outcomes);
+				fall15Spark.set("sustainability", sustainability);
+				fall15Spark.set("budget", budget);
+				fall15Spark.set("amount", amount);
+
+				fall15Spark.save(null, {
+				  success: function(fall15Spark) {
+				    console.log('save successful');
+				    console.log(fall15Spark);
+				  },
+				  error: function(fall15Spark, error) {
+				    console.log('Failed to create new object, with error code: ' + error.description);
+				  }
+				});
+				$('.spark-application-form')[0].reset();
+			} else {
+				$('.spark-error-msg').show();
+				$('.spark-success-msg').remove();
+			}
+		});
 });
